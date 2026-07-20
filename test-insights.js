202b949dbdca6ff7336cc8cce5a663ef1fef674e
Mika <mika@MacBook-Air-Mihail-2.local>
2026-07-20 12:01:09 +0300

    chore: remove local testing artifacts

--- test-insights.js
+++
@@ -1,8 +0,0 @@
-const { mockFeedbackData } = require('./src/lib/mockFeedbackData.js');
-
-async function test() {
-  console.log("Mock data exists:", !!mockFeedbackData);
-  // Actually, I can just hit the local API since `npm run dev` might be running.
-  // Let me just test the prompt generation.
-}
-test();
